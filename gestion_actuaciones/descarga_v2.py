ed051bf15678e67531f2ffeb835646967101a564
davidliva1378 <130333981+davidliva1378@users.noreply.github.com>
2025-07-28 14:18:22 -0300

    Add entry point for menu interface

---
+++ gestion_actuaciones/descarga_v2.py
@@ -0,0 +1,1 @@
+from panel_pjn.acciones_pjn.gestion_actuaciones.descarga_v2 import *
---
+++ gestion_expedientes/__init__.py
@@ -0,0 +1,3 @@
+"""Alias para los módulos de gestión de expedientes."""
+from panel_pjn.acciones_pjn.gestion_expedientes.busqueda_expediente import *  # noqa: F401,F403
+from panel_pjn.acciones_pjn.gestion_expedientes.mostrar_y_elegir_expediente import *  # noqa: F401,F403
