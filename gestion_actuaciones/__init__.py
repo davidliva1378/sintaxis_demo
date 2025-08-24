642c0726cc347838c4f705e3b356956eb6f7cb37
David Liva <davidliva@gmail.com>
2025-08-24 16:40:05 -0300

    reordenando codigo: extraccion de exptes y entradas - eliminando gestion de expedientes y actuaciones (correctas en panel_pjn)

--- gestion_actuaciones/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Alias para los módulos de gestión de actuaciones."""
-from panel_pjn.acciones_pjn.gestion_actuaciones.extraccion_v2 import *  # noqa: F401,F403
-from panel_pjn.acciones_pjn.gestion_actuaciones.descarga_v2 import *  # noqa: F401,F403
-from panel_pjn.acciones_pjn.gestion_actuaciones.extraccion_completa import *  # noqa: F401,F403
--- gestion_actuaciones/descarga_v2.py
+++
@@ -1,1 +0,0 @@
-from panel_pjn.acciones_pjn.gestion_actuaciones.descarga_v2 import *
