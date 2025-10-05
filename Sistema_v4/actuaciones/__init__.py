85cf9dda722e42caffaba90830fdc662ecf1fe38
davidliva1378 <130333981+davidliva1378@users.noreply.github.com>
2025-10-05 02:21:42 -0300

    Add actuaciones module for Sistema v4

---
+++ Sistema_v4/actuaciones/__init__.py
@@ -0,0 +1,21 @@
+from .actuaciones_v4 import (
+    aviso_si_tarda,
+    construir_nombre_archivo_normalizado,
+    descargar_archivos_actuaciones,
+    descargar_archivos_de_json,
+    extraer_actuaciones_completas,
+    extraer_actuaciones_historicas,
+    extraer_actuaciones_pagina,
+    obtener_actuaciones_todas_paginas_async,
+)
+
+__all__ = [
+    "aviso_si_tarda",
+    "construir_nombre_archivo_normalizado",
+    "descargar_archivos_actuaciones",
+    "descargar_archivos_de_json",
+    "extraer_actuaciones_completas",
+    "extraer_actuaciones_historicas",
+    "extraer_actuaciones_pagina",
+    "obtener_actuaciones_todas_paginas_async",
+]
