09aa3f963ed33bd846b165f46fe01b60da1e794c
davidliva1378 <130333981+davidliva1378@users.noreply.github.com>
2025-10-22 19:24:23 -0300

    Add services to extract initial expediente listings

--- extractor_inicial/__init__.py
+++ extractor_inicial/__init__.py
@@ -1,6 +1,11 @@
 """Paquete de coordinación del extractor inicial."""
-
 from .ciclo_prueba import run_ciclo_prueba
+from .services import extraer_listado_inicial, obtener_listado_inicial
 from .ui.directorios_form import mostrar_formulario_directorios
 
-__all__ = ["run_ciclo_prueba", "mostrar_formulario_directorios"]
+__all__ = [
+    "run_ciclo_prueba",
+    "mostrar_formulario_directorios",
+    "extraer_listado_inicial",
+    "obtener_listado_inicial",
+]
