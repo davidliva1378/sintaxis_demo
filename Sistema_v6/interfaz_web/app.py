ab3a5b0e943aaa00cd6a0287f86ac7b3579a3c6f
davidalejandroliva <davidliva@gmail.com>
2025-11-07 03:55:46 -0300

    mejoras api

--- Sistema_v6/interfaz_web/app.py
+++ Sistema_v6/interfaz_web/app.py
@@ -6,9 +6,9 @@ from backend.main import app
 
 
 def iniciar_backend():
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
 
 if __name__ == "__main__":
     threading.Thread(target=iniciar_backend, daemon=True).start()
-    webview.create_window("Sistema Jurídico", "http://127.0.0.1:8000")
+    webview.create_window("Sistema Jurídico", "http://127.0.0.1:8001")
     webview.start()
